764c819c41f46482225bec8ace93ec99b08b9f93
max52killer <songzm@dragoninfo.com.cn>
2019-12-22 11:56:09 +0800

    新增字段设置初始值

--- test_demo/src/pages/a-form/index.vue
+++ test_demo/src/pages/a-form/index.vue
@@ -1,19 +1,27 @@
 <template>
   <div>
-    <a-form :form="form"  @submit="getValue">
+    <a-form :form="form" @submit="getValue">
       <a-form-item
         :labelCol="{lg:{span:7},sm:{span:7}}"
         :wrapperCol="{lg:{span:10},sm:{span:17}}"
         label="姓名："
       >
-        <a-input v-decorator="['name',{rules: [{ required: true, message: '请输入客户名称!' }]}]"></a-input>
+        <a-input
+          v-decorator="['name',
+        {
+          rules: [
+            { required: true, message: '请输入客户名称!' }, 
+          ]
+        }]"
+        ></a-input>
       </a-form-item>
       <a-form-item
         :labelCol="{lg:{span:7},sm:{span:7}}"
         :wrapperCol="{lg:{span:10},sm:{span:17}}"
         label="性别："
       >
-      <a-input v-decorator="['sex']"></a-input>
+        <a-input v-decorator="['age',{initialValue:age}]" v-show="false"></a-input>
+        <a-input v-decorator="['sex']"></a-input>
       </a-form-item>
       <a-form-item :labelCol="{lg:{span:7},sm:{span:7}}" :wrapperCol="{lg:{span:10},sm:{span:17}}">
         <a-button type="primary" @click="getValue">取表单值</a-button>
@@ -25,19 +33,22 @@
 <script>
 export default {
   name: "",
-  activited() {},
+  created() {
+    this.setValue();
+  },
   data() {
     return {
       vueValue: "",
-      form: this.$form.createForm(this) // 只有这样注册后，才能通过表单拉取数据
+      form: this.$form.createForm(this), // 只有这样注册后，才能通过表单拉取数据
+      age:19,
     };
   },
   methods: {
     setValue() {
-      this.form.setFieldsValue({
-        name: "张三",
-        sex: "男"
-      });
+      // this.form.setFieldsValue({
+      //   name: "张三",
+      //   sex: "男",
+      // });
     },
     getValue() {
       let listV = this.form.getFieldsValue();
